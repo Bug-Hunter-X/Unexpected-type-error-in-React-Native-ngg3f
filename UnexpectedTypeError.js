418f8f97073144afaf49d2481f55616ffb1679c1
BugHunter-X <askzenai@gmail.com>
2024-12-13 05:24:06 +0100

    Add UnexpectedTypeError.js file

---
+++ UnexpectedTypeError.js
@@ -0,0 +1,1 @@
+This error occurs when you use a component that expects a specific prop type, but you provide it with a different type or a value that cannot be handled properly. For example, if a component expects a number prop, and you pass it a string, this error will occur. Another cause is passing null or undefined to a prop that does not accept it.
